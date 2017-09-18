dafd9c5902f264dca0c0d2910f576b2dacfabc7d
Sean Kelly <sean.kelly@edu.sait.ca>
2017-09-18 09:28:11 -0600

    Prints a "Hello World" to console

--- src/week01lab/Week01Lab.java
+++ src/week01lab/Week01Lab.java
@@ -16,6 +16,7 @@ public class Week01Lab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
